cf2849ff851e9ac2452662749ccad3b0179575f3
Vishnu S <70127764+vishnurocking@users.noreply.github.com>
2024-07-19 12:14:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Vishnu successfully hosted a React web app using AWS!</p>
+        <p>Prgrammer Vishnu successfully hosted a React web app using AWS Amplify!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
